8d577f74a473eef849f710a436ed7e17cfd7905d
Augustusisblue <71149146+Augustusisblue@users.noreply.github.com>
2020-09-25 17:39:00 +0300

    Source
    just the source code, nothing rlly special

---
+++ simpleguessinggame.py
@@ -0,0 +1,30 @@
+# Most basic shit ever
+# simplest functions used
+# still cool when you know that I just learnt python an hour ago
+# I like coding
+import random
+on = input()
+off = "off"
+start1 = "start"
+x = random.randint(1,9)
+ #add random randint functions somehow???
+guess = -1
+
+if on == "on":
+    start = input("Type start, to turn off type off: ")
+    if start1:
+        while start:
+            y = (input("Type anything to commence:"))
+            if y == "off":
+                break
+            else:
+                from random import randint
+
+                print("Guess the number below 10:")
+                while guess != x:
+                    guess = int(input("Guess: "))
+
+                    if guess != x:
+                        print("Wrong guess retard, no one likes you because of how dumb you are, ذيل نزار")
+                    else:
+                        print("Guessed correctly, good job")
